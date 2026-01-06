b4b6db90cedf1060ab16cd8e3871ea9acd7c361d
Irisu <mrapacheee@gmail.com>
2026-01-06 15:49:51 +0400

    Remove unnecessary code

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -9,19 +9,15 @@ pub(crate) use debug::draw_collider_debug_lines;
 pub use rigidbody::KinematicRigidBody;
 pub use velocity::Velocity;
 
-use crate::{fx, transform::Position};
+use crate::transform::Position;
 pub mod prelude {
     pub use super::{KinematicRigidBody, PhysicsDebugManager, Velocity, collision::prelude::*};
 }
 
-pub(crate) fn apply_velocity(
-    time: Res<Time<Fixed>>,
-    mut entities: Query<(&mut Position, &mut KinematicRigidBody)>,
-) {
-    for (mut position, mut rigid_body) in &mut entities {
+pub(crate) fn apply_velocity(mut entities: Query<(&mut Position, &KinematicRigidBody)>) {
+    for (mut position, rigid_body) in &mut entities {
         position.x += rigid_body.velocity.x;
         position.y += rigid_body.velocity.y;
         position.z += rigid_body.velocity.z;
-        //rigid_body.velocity.y -= fx::from_num(10.0 * time.delta_secs());
     }
 }
