38e5a03b925f0c52316386b616f00e91a6a25b85
Irisu <mrapacheee@gmail.com>
2026-01-06 16:03:41 +0400

    Fix velocity application

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -3,21 +3,25 @@ mod debug;
 mod rigidbody;
 mod velocity;
 
-use bevy::prelude::*;
+use bevy::{log::tracing_subscriber::field::delimited::Delimited, prelude::*};
 pub use debug::PhysicsDebugManager;
 pub(crate) use debug::draw_collider_debug_lines;
 pub use rigidbody::KinematicRigidBody;
 pub use velocity::Velocity;
 
-use crate::transform::Position;
+use crate::{fx, transform::Position};
 pub mod prelude {
     pub use super::{KinematicRigidBody, PhysicsDebugManager, Velocity, collision::prelude::*};
 }
 
-pub(crate) fn apply_velocity(mut entities: Query<(&mut Position, &KinematicRigidBody)>) {
+pub(crate) fn apply_velocity(
+    time: Res<Time<Fixed>>,
+    mut entities: Query<(&mut Position, &KinematicRigidBody)>,
+) {
+    let delta = fx::from_num(time.delta_secs());
     for (mut position, rigid_body) in &mut entities {
-        position.x += rigid_body.velocity.x;
-        position.y += rigid_body.velocity.y;
-        position.z += rigid_body.velocity.z;
+        position.x += rigid_body.velocity.x * delta;
+        position.y += rigid_body.velocity.y * delta;
+        position.z += rigid_body.velocity.z * delta;
     }
 }
